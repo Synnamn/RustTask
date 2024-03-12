4268a3afc5a28e700ee2a29bf0584e653bf478c2
Synnamn <symony@ebnett.no>
2024-03-12 15:59:50 +0100

    Create Project
    Setting up VS Code Project

---
+++ my_rust_task/target/debug/deps/my_rust_task-1f2062300bcc9f47.d
@@ -0,0 +1,5 @@
+D:\Documents\GitHub\RustTask\my_rust_task\target\debug\deps\libmy_rust_task-1f2062300bcc9f47.rmeta: src\main.rs
+
+D:\Documents\GitHub\RustTask\my_rust_task\target\debug\deps\my_rust_task-1f2062300bcc9f47.d: src\main.rs
+
+src\main.rs:
---
+++ my_rust_task/target/debug/deps/my_rust_task-3bfbb3e640cc268c.d
@@ -0,0 +1,5 @@
+D:\Documents\GitHub\RustTask\my_rust_task\target\debug\deps\libmy_rust_task-3bfbb3e640cc268c.rmeta: src\main.rs
+
+D:\Documents\GitHub\RustTask\my_rust_task\target\debug\deps\my_rust_task-3bfbb3e640cc268c.d: src\main.rs
+
+src\main.rs:
